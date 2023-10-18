58c426ec3ee947ab88146eca61ffb412acebf17e
Jesse Smith <putamadre@jesses-mbp.lan>
2023-10-18 14:03:12 -0400

    initial commit with boiler plate and nav menu created

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './layout/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+    './templates/customers/*.liquid',
+    './templates/*.liquid'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
